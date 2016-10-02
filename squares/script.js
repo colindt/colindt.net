35db039ce64d19352c2668df968f7e9368b5b0de
colindt <cdt1_2@msn.com>
2016-10-02 05:07:14 -0700

    click squares to cycle colors

--- squares/script.js
+++ squares/script.js
@@ -1,15 +1,22 @@
 "use strict";
 
 
-function reroll() {
+function getClasses() {
     var classes = ["colorW", "color1", "color2", "color3", "color4"];
-
-    if (!document.getElementById("white").checked) {
-        classes = classes.slice(1);
+    if (document.getElementById("white").checked) {
+        return classes.slice();
     }
+    else {
+        return classes.slice(1);
+    }
+}
+
+
+function reroll() {
+    var classes = getClasses();
 
     var remaining = [];
-    for (var i in classes) {
+    for (var i = 0; i < classes.length; i++) {
         for (var j = 0; j < 80 / classes.length; j++) {
             remaining.push(classes[i]);
         }
@@ -34,9 +41,27 @@ function toggleLabels() {
 }
 
 
+function cycleColor(elem) {
+    var classes = getClasses();
+    var next = {};
+    for (var i = 0; i < classes.length-1; i++) {
+        next[classes[i]] = classes[i+1];
+    }
+    next[classes[classes.length-1]] = classes[0];
+
+    elem.className = next[elem.className];
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("reroll").addEventListener("click", reroll);
     document.getElementById("labels").addEventListener("click", toggleLabels);
+
+    var cells = document.getElementsByTagName("td");
+    function click(elem) {return function () {cycleColor(elem);}}
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", click(cells[i]));
+    }
 
     reroll();
     toggleLabels();
